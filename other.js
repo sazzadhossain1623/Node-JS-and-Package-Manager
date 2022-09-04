ae784b1a60c56e02b221c8bb98810118d180db83
Sazzad Mahmud <mahmudsazzad85@gmail.com>
2022-09-04 15:45:08 +0600

    request and response model

---
+++ other.js
@@ -0,0 +1,11 @@
+var name = "Programming Hero";
+
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+const substract = (num1, num2) => {
+    return num1 - num2;
+}
+
+module.exports={name, add, substract};
